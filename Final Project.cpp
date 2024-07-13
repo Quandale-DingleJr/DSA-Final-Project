48f2dfb321c86d1b8dd883f94451581acef28fe3
Quandale-DingleJr <202311159@fit.edu.ph>
2024-07-13 14:27:26 +0800

    Add files via upload

--- Final Project.cpp
+++ Final Project.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 #include <string>
-#include <list>
+#include <vector>
 #include <fstream>
 
 using namespace std;
 
 class Book{
 	private://data
-		list<int> bookID;
-		list<int> nofCopies;
-		list<string> bookTitle;
-		list<string> author;
-		list<string> genre;
+		vector<int> bookID;
+		vector<int> nofCopies;
+		vector<string> bookTitle;
+		vector<string> author;
+		vector<string> genre;
 	
 	public:
 		//no constructor
 		void insertBook(){
-			int id, copies;
+			int id = bookID.size() + 1;
+			int copies;
 			string title, auth, genr;
 			
-			cout << "Book ID:\t";
-			cin >> id;
+			cout << "Book ID:\t" << id << endl;
 			bookID.push_back(id);
-			cin.ignore();
 			
 			cout << "Book Title:\t";
+			cin.ignore();
 			getline(cin, title);
 			bookTitle.push_back(title);
 			
@@ -43,15 +43,61 @@ class Book{
 			}
 			
 		void rentBook(){
-			int id, copies;
-			string title, auth, genr;
+			int id;
+			char choice;
 			
-			cout << "Book ID:\t";
-			cin >> id;
-
+			do{
+				cout << "Book ID:\t";
+				cin >> id;
+				
+				bool found = false;
+				
+				for(size_t i = 0; i < bookID.size(); i++){
+					if(id == bookID[i]){
+						cout << "Book Title:\t" << bookTitle[i] << endl;
+						cout << "Number of Copies:\t" << nofCopies[i] << endl;
+						found = true;
+						break;
+					}
+				}
+				if(!found){
+					cout << "Book not Found...";
+				}
+				cout << "Rent another book?(Y/N): ";
+				cin >> choice;
+			}while(choice == 'Y' || choice == 'y');
 		}
-};
-int main(){
+		
+		void returnBook() {
+      	 		 int id;
+       			 cout << "Book ID:\t";
+        		 cin >> id;
+
+      			  vector<int>::iterator itID = bookID.begin();
+      			  vector<int>::iterator itCopies = nofCopies.begin();
+        		  bool found = false;
+
+      		 	 while (itID != bookID.end()) {
+           	 		if (*itID == id) {
+               		 	found = true;
+              	 	 	(*itCopies)++;
+              		  	cout << "Book returned successfully.\n";
+              	 	 	break;
+          	  		}
+          	 	 ++itID;
+         	  	 ++itCopies;
+       		 	}
+	
+      		  if (!found) {
+      		      cout << "Book ID not found.\n";
+      		  }
+   	 	}
+			
+		void showBookDetails() {
+			}
+		void displayBooks(){
+		}
+			int main(){
 
 	
 }
